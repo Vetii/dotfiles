bde2451dc097fbbaa67d63411319bdc976f6f51e
Noric Couderc <noric.couderc@gmail.com>
2019-10-02 11:05:54 +0200

    Adding Zotero in the path if its directory exists
    If zotero is installed, we add it to the PATH

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,8 @@ alias fullpath 'readlink -f'
 
 # OPAM configuration
 source /home/noric/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+
+# Adding Zotero to the path
+if test -e /opt/zotero/zotero
+    set PATH /opt/zotero/ $PATH
+end
